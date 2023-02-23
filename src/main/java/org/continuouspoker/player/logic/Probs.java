0cf41f366e164d59e5a87069c9348c9b78a53c9c
Tobias Schmid <tobias.schmid@buit-solutions.com>
2023-02-23 20:54:38 +0100

    props

---
+++ src/main/java/org/continuouspoker/player/logic/Probs.java
@@ -0,0 +1,14 @@
+package org.continuouspoker.player.logic;
+
+public class Probs {
+    public static float Royal = 0.0002F;
+    public static float StraightFlush = 0.0014F;
+    public static float Four = 0.0024F;
+    public static float House = 0.1441F;
+    public static float Flush = 0.1965F;
+    public static float Straight = 0.3925F;
+    public static float Three = 2.1128f;
+    public  static float Two = 4.7539F;
+    public  static float High = 42.2569F;
+    public static  float None = 50.1177F;
+}
